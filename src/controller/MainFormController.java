6ba83ba71a22673a0bce8ad806b33db9a18b6fd6
Dhanushka Chandimal <dhanushkachandimal11@gmail.com>
2021-06-15 00:35:02 +0530

    Change accessing the forms using AccessibleText and used form url array

--- src/controller/MainFormController.java
+++ src/controller/MainFormController.java
@@ -16,84 +16,59 @@ public class MainFormController {
     public AnchorPane pneStage;
     public Label lblTitle;
 
-    Parent[] formArray = new Parent[6];
+    Parent[] formArray;
 
     public void initialize() throws IOException {
-        formArray[0] = FXMLLoader.load(this.getClass().getResource("../view/DashboardForm.fxml"));
-        formArray[1] = FXMLLoader.load(this.getClass().getResource("../view/AddNewPaymentForm.fxml"));
-        formArray[2] = FXMLLoader.load(this.getClass().getResource("../view/ViewPaymentsForm.fxml"));
-        formArray[3] = FXMLLoader.load(this.getClass().getResource("../view/ManageStudentsForm.fxml"));
-        formArray[4] = FXMLLoader.load(this.getClass().getResource("../view/ViewCoursesForm.fxml"));
-        formArray[5] = FXMLLoader.load(this.getClass().getResource("../view/ViewBatchesForm.fxml"));
+        String[] urlFormNames={"DashboardForm","AddNewPaymentForm","ViewPaymentsForm","ManageStudentsForm","ViewCoursesForm","ViewBatchesForm"};
+        formArray = new Parent[urlFormNames.length];
+        for (int i = 0; i < formArray.length; i++) {
+            formArray[i] = FXMLLoader.load(this.getClass().getResource("../view/"+urlFormNames[i]+".fxml"));
+        }
     }
 
     public void pneDashboard_OnMouseClicked(MouseEvent mouseEvent) {
-        //loadForm("../view/DashboardForm.fxml", "Dashboard", "Dashboard");
-        load(0, "Dashboard", "Dashboard");
+        load(0, "Dashboard");
     }
 
     public void pneAddNewPayment_OnMouseClicked(MouseEvent mouseEvent) {
-        //loadForm("../view/AddNewPaymentForm.fxml", "Add New Payment", "Dashboard / Add New Payment");
-        load(1, "Add New Payment", "Dashboard / Add New Payment");
+        load(1, "Dashboard / Add New Payment");
     }
 
     public void pneViewPayments_OnMouseClicked(MouseEvent mouseEvent) {
-        //loadForm("../view/ViewPaymentsForm.fxml", "View Payments", "Dashboard / View Payments");
-        load(2, "View Payments", "Dashboard / View Payments");
+        load(2, "Dashboard / View Payments");
     }
 
     public void pneManageStudents_OnMouseClicked(MouseEvent mouseEvent) {
-        //loadForm("../view/ManageStudentsForm.fxml", "Manage Students", "Dashboard / Manage Students");
-        load(3, "Manage Students", "Dashboard / Manage Students");
+        load(3, "Dashboard / Manage Students");
     }
 
     public void pneViewCourses_OnMouseClicked(MouseEvent mouseEvent) {
-        //loadForm("../view/ViewCoursesForm.fxml", "View Courses", "Dashboard / View Courses");
-        load(4, "View Courses", "Dashboard / View Courses");
+        load(4, "Dashboard / View Courses");
     }
 
     public void pneViewBatches_OnMouseClicked(MouseEvent mouseEvent) {
-        //loadForm("../view/ViewBatchesForm.fxml", "View Batches", "Dashboard / View Batches");
-        load(5, "View Batches", "Dashboard / View Batches");
+        load(5, "Dashboard / View Batches");
     }
 
-    private void load(int url, String formName, String currentFormLocation) { // Make sure to update AccessibleText in all main anchor panes
-        String accessibleText = pneStage.getChildren().get(0).getAccessibleText();
-        if (accessibleText != null && accessibleText.equals(formName)) {
+    private void load(int urlIndex, String currentFormLocation) {
+        //String accessibleText = pneStage.getChildren().get(0).getAccessibleText();
+        System.out.println(pneStage.getChildren().get(0));
+        System.out.println(formArray[urlIndex]);
+        System.out.println();
+        if (pneStage.getChildren().get(0)==formArray[urlIndex]) {
             return;
         }
         pneStage.getChildren().clear();
-        pneStage.getChildren().add(formArray[url]);
+        pneStage.getChildren().add(formArray[urlIndex]);
         Stage mainStage = (Stage) pneStage.getScene().getWindow();
-        mainStage.setTitle(formName);
+        String[] splittedFormLocationString = currentFormLocation.split("/");
+        mainStage.setTitle(splittedFormLocationString[splittedFormLocationString.length-1].trim());
         lblTitle.setText(currentFormLocation);
         Platform.runLater(() -> {
             //mainStage.setMaximized(true);
             mainStage.centerOnScreen();
             mainStage.sizeToScene();
         });
-    }
-
-    private void loadForm(String url, String formName, String currentFormLocation) { // Make sure to update AccessibleText in all main anchor panes
-        String accessibleText = pneStage.getChildren().get(0).getAccessibleText();
-        if (accessibleText != null && accessibleText.equals(formName)) {
-            return;
-        }
-        pneStage.getChildren().clear();
-        try {
-            Parent formLoader = FXMLLoader.load(this.getClass().getResource(url));
-            pneStage.getChildren().add(formLoader);
-            Stage mainStage = (Stage) pneStage.getScene().getWindow();
-            mainStage.setTitle(formName);
-            lblTitle.setText(currentFormLocation);
-            Platform.runLater(() -> {
-                //mainStage.setMaximized(true);
-                mainStage.centerOnScreen();
-                mainStage.sizeToScene();
-            });
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
     public void imgSettings_OnMouseClicked(MouseEvent mouseEvent) throws IOException {
