f53829502c0e6943709d60e0462dbe6176963410
Dhanushka Chandimal <dhanushkachandimal11@gmail.com>
2021-06-14 18:01:36 +0530

    Design ViewPaymentsForm and Create class ViewPaymentFormController

---
+++ src/controller/ViewPaymentsFormController.java
@@ -0,0 +1,30 @@
+package controller;
+
+import com.jfoenix.controls.JFXComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+
+public class ViewPaymentsFormController {
+    public ToggleGroup rbtnDayGaps;
+    public TextField txtFromDate;
+    public TextField txtReceiptNumber;
+    public TextField txtNIC;
+    public JFXComboBox cmbCourseId;
+    public JFXComboBox cmbBatchNumber;
+    public TextField txtPaymentDescription;
+    public TextField txtPaymentMethod;
+    public TextField txtOnlineReference;
+    public TextField txtCardNumber;
+    public TextField txtExpirationDate;
+    public TextField txtNameOnCard;
+    public TextField txtTotalAmount;
+    public TextField txtAmountReceived;
+    public TextField txtBalanceAmount;
+    public TextField txtDueDate;
+    public TextField txtBalanceSettledDate;
+    public TextField txtReceiptDate;
+    public TextField txtCashier;
+    public TextField txtNotes;
+    public TextField txtSearch;
+    public TextField txtToDate;
+}
