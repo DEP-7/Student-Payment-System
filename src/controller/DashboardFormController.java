f0db94a08da6b76a69b29cc94f5978a2d5c6ba2d
Dhanushka Chandimal <dhanushkachandimal11@gmail.com>
2021-06-19 01:29:32 +0530

    Add some styles to table in dashboard form

--- src/controller/DashboardFormController.java
+++ src/controller/DashboardFormController.java
@@ -1,4 +1,13 @@
 package controller;
 
+import javafx.scene.control.TableView;
+
 public class DashboardFormController {
+    public TableView tblResults;
+
+    public void initialize(){
+        tblResults.getItems().add(new String("sdfsd"));
+        tblResults.getItems().add(new String("sdfsd"));
+        tblResults.getItems().add(new String("sdfsd"));
+    }
 }
