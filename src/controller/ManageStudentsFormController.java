b936afbcd6c534a4a6d6d06d6ffa59494390e571
Dhanushka Chandimal <dhanushkachandimal11@gmail.com>
2021-06-20 01:00:08 +0530

    Create a method to add '-' automatically in date of birth and contact number and add letter V as last letter in NIC

--- src/controller/ManageStudentsFormController.java
+++ src/controller/ManageStudentsFormController.java
@@ -5,6 +5,8 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import model.Student;
 import model.StudentTM;
 import service.StudentService;
@@ -13,8 +15,10 @@ import util.MaterialUI;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class ManageStudentsFormController {
+    private final StudentService studentService = new StudentService();
     public TableView<StudentTM> tblResult;
     public JFXComboBox<String> cmbBatchNumber;
     public JFXComboBox<String> cmbCourseId;
@@ -32,7 +36,9 @@ public class ManageStudentsFormController {
     public TextField txtEmail;
     public TextField txtNIC;
     public TextField txtAge;
-    private final StudentService studentService = new StudentService();
+    TextField previouslyFocusedTextField;
+    ArrayList<Integer> caretPositionArray;
+    int lengthOfString = 0;
 
     public void initialize() {
         cmbBatchNumber.getItems().add("001");
@@ -57,7 +63,8 @@ public class ManageStudentsFormController {
     }
 
     public void btnAdd_OnAction(ActionEvent actionEvent) {
-        trimAllTextFields(txtNIC, txtAddress, txtContactNumber, txtEmail, txtDateOfBirth, txtFullName, txtNameWithInitials, txtHighestEducationalQualification);
+        trimTextFields(txtNIC, txtEmail, txtAddress, txtFullName, txtDiscount, txtContactNumber, txtNameWithInitials, txtHighestEducationalQualification);
+
         Student student = new Student(txtNIC.getText(),
                 txtFullName.getText(),
                 txtNameWithInitials.getText(),
@@ -71,6 +78,7 @@ public class ManageStudentsFormController {
                 cmbCourseId.getValue(),
                 Integer.parseInt(cmbBatchNumber.getValue()),
                 new BigDecimal(txtDiscount.getText()));
+
         try {
             studentService.addStudent(student);
             Alert alertSuccess = new Alert(Alert.AlertType.INFORMATION, "Student have been saved successfully", ButtonType.OK);
@@ -84,9 +92,79 @@ public class ManageStudentsFormController {
         }
     }
 
-    private void trimAllTextFields(TextField... textFields) {
+    private void trimTextFields(TextField... textFields) {
         for (TextField textField : textFields) {
             textField.setText(textField.getText().trim());
         }
     }
+
+    public void txtContactNumber_OnKeyReleased(KeyEvent keyEvent) {
+        separateString(keyEvent, txtContactNumber, "-", 3, 7);
+    }
+
+    public void txtNIC_OnKeyReleased(KeyEvent keyEvent) {
+        if (txtNIC.getText().length() > 10) {
+            int caretPosition = txtNIC.getCaretPosition();
+            txtNIC.setText(txtNIC.getText().replaceAll("[Vv]", ""));
+            txtNIC.positionCaret(caretPosition);
+            separateString(keyEvent, txtNIC, "", 12);
+            return;
+        }
+        separateString(keyEvent, txtNIC, "V", 9);
+    }
+
+    public void txtDateOfBirth_OnKeyReleased(KeyEvent keyEvent) {
+        separateString(keyEvent, txtDateOfBirth, "-", 4, 2, 2);
+    }
+
+    private void separateString(KeyEvent keyEvent, TextField currentTextField, String separator, int... separatingLengths) {
+        String text = currentTextField.getText();
+        int caretPosition = currentTextField.getCaretPosition();
+        boolean isCaretAtLast = currentTextField.getText().length() == caretPosition;
+
+        if (keyEvent.getCode() == KeyCode.BACK_SPACE) {
+            if (caretPositionArray.contains(caretPosition)) {
+                text = text.substring(0, caretPosition - 1) + text.substring(caretPosition);
+                caretPosition -= 1;
+            }
+        }
+        text = text.replaceAll("[^0-9]", "");
+        lengthOfString = previouslyFocusedTextField == currentTextField ? lengthOfString : calculateLengthOfString(separatingLengths); // Reduce iteration when typing in same text field
+        String newText = "";
+        int indexTotal = 0;
+        int indexTotalPrevious = 0;
+
+        for (int i = 0; i < separatingLengths.length; i++) {
+            indexTotalPrevious += separatingLengths[i];
+            if (text.length() > indexTotal) {
+                if (text.length() >= indexTotalPrevious) {
+                    newText += text.substring(indexTotal, indexTotalPrevious) + separator;
+                } else {
+                    newText += text.substring(indexTotal);
+                    break;
+                }
+                indexTotal += separatingLengths[i];
+            } else {
+                break;
+            }
+        }
+
+        if (separatingLengths.length>1 && keyEvent.getText().matches("\\d") && separator.equals(Character.toString(newText.charAt(caretPosition - 1)))) { // 1993|-5 -> 1993-|25 caret position error fixing 1993-2|5
+            caretPosition += 1;
+        }
+        currentTextField.setText(lengthOfString <= text.length() && separatingLengths.length != 1 ? newText.substring(0, newText.length() - 1) : newText);
+        currentTextField.positionCaret(isCaretAtLast ? newText.length() : caretPosition);
+        previouslyFocusedTextField = currentTextField;
+    }
+
+    private int calculateLengthOfString(int[] integerArray) {
+        int total = 0;
+        caretPositionArray = new ArrayList<>();
+        for (int i = 0; i < integerArray.length; i++) {
+            total += integerArray[i];
+            caretPositionArray.add(total + i);
+        }
+        return total;
+    }
+
 }
--- src/service/StudentService.java
+++ src/service/StudentService.java
@@ -12,14 +12,14 @@ import java.util.List;
 public class StudentService {
     public static ArrayList<Student> studentDB = new ArrayList();
 
-//    static {
-//        studentDB.add(new Student("931630377V", "Magam Mudalige Dhanushka Chandimal Ranasinghe", "M.M.D.C.Ranasinghe", "Male", LocalDate.of(1993, 6, 11), null, "A/L passed", "Handiya Kade, Deeyagaha, Matara", "0716520080", "dhanushkachandimal11@gmail.com", "DEP", 7, new BigDecimal("0")));
-//        studentDB.add(new Student("931630377V", "Magam Mudalige Dhanushka Chandimal Ranasinghe", "M.M.D.C.Ranasinghe", "Male", LocalDate.of(1993, 6, 11), null, "A/L passed", "Handiya Kade, Deeyagaha, Matara", "0716520080", "dhanushkachandimal11@gmail.com", "DEP", 7, new BigDecimal("0")));
-//        studentDB.add(new Student("931630377V", "Magam Mudalige Dhanushka Chandimal Ranasinghe", "M.M.D.C.Ranasinghe", "Male", LocalDate.of(1993, 6, 11), null, "A/L passed", "Handiya Kade, Deeyagaha, Matara", "0716520080", "dhanushkachandimal11@gmail.com", "DEP", 7, new BigDecimal("0")));
-//        studentDB.add(new Student("931630377V", "Magam Mudalige Dhanushka Chandimal Ranasinghe", "M.M.D.C.Ranasinghe", "Male", LocalDate.of(1993, 6, 11), null, "A/L passed", "Handiya Kade, Deeyagaha, Matara", "0716520080", "dhanushkachandimal11@gmail.com", "DEP", 7, new BigDecimal("0")));
-//        studentDB.add(new Student("931630377V", "Magam Mudalige Dhanushka Chandimal Ranasinghe", "M.M.D.C.Ranasinghe", "Male", LocalDate.of(1993, 6, 11), null, "A/L passed", "Handiya Kade, Deeyagaha, Matara", "0716520080", "dhanushkachandimal11@gmail.com", "DEP", 7, new BigDecimal("0")));
-//        studentDB.add(new Student("931630377V", "Magam Mudalige Dhanushka Chandimal Ranasinghe", "M.M.D.C.Ranasinghe", "Male", LocalDate.of(1993, 6, 11), null, "A/L passed", "Handiya Kade, Deeyagaha, Matara", "0716520080", "dhanushkachandimal11@gmail.com", "DEP", 7, new BigDecimal("0")));
-//    }
+    static {
+        studentDB.add(new Student("931630377V", "Magam Mudalige Dhanushka Chandimal Ranasinghe", "M.M.D.C.Ranasinghe", "Male", LocalDate.of(1993, 6, 11), null, "A/L passed", "Handiya Kade, Deeyagaha, Matara", "0716520080", "dhanushkachandimal11@gmail.com", "DEP", 7, new BigDecimal("0")));
+        studentDB.add(new Student("931630377V", "Magam Mudalige Dhanushka Chandimal Ranasinghe", "M.M.D.C.Ranasinghe", "Male", LocalDate.of(1993, 6, 11), null, "A/L passed", "Handiya Kade, Deeyagaha, Matara", "0716520080", "dhanushkachandimal11@gmail.com", "DEP", 7, new BigDecimal("0")));
+        studentDB.add(new Student("931630377V", "Magam Mudalige Dhanushka Chandimal Ranasinghe", "M.M.D.C.Ranasinghe", "Male", LocalDate.of(1993, 6, 11), null, "A/L passed", "Handiya Kade, Deeyagaha, Matara", "0716520080", "dhanushkachandimal11@gmail.com", "DEP", 7, new BigDecimal("0")));
+        studentDB.add(new Student("931630377V", "Magam Mudalige Dhanushka Chandimal Ranasinghe", "M.M.D.C.Ranasinghe", "Male", LocalDate.of(1993, 6, 11), null, "A/L passed", "Handiya Kade, Deeyagaha, Matara", "0716520080", "dhanushkachandimal11@gmail.com", "DEP", 7, new BigDecimal("0")));
+        studentDB.add(new Student("931630377V", "Magam Mudalige Dhanushka Chandimal Ranasinghe", "M.M.D.C.Ranasinghe", "Male", LocalDate.of(1993, 6, 11), null, "A/L passed", "Handiya Kade, Deeyagaha, Matara", "0716520080", "dhanushkachandimal11@gmail.com", "DEP", 7, new BigDecimal("0")));
+        studentDB.add(new Student("931630377V", "Magam Mudalige Dhanushka Chandimal Ranasinghe", "M.M.D.C.Ranasinghe", "Male", LocalDate.of(1993, 6, 11), null, "A/L passed", "Handiya Kade, Deeyagaha, Matara", "0716520080", "dhanushkachandimal11@gmail.com", "DEP", 7, new BigDecimal("0")));
+    }
 
     public void addStudent(Student student) throws DuplicateEntryException {
         if (getStudent(student.getNic()) != null) {
