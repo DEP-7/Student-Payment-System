19b7bb9f95be548953827ed7a189b65be4abb807
Dhanushka Chandimal <dhanushkachandimal11@gmail.com>
2021-07-02 07:25:34 +0530

    Add splash screen

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -1,8 +1,10 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -14,13 +16,22 @@ public class AppInitializer extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        Parent root = FXMLLoader.load(this.getClass().getResource("view/LoginForm.fxml"));
-//        Parent root = FXMLLoader.load(this.getClass().getResource("view/MainForm.fxml"));
-        Scene loginScene = new Scene(root);
-        primaryStage.setScene(loginScene);
-        primaryStage.setTitle("Login");
-        primaryStage.setResizable(false);
+        AnchorPane root = FXMLLoader.load(this.getClass().getResource("view/SplashScreenForm.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        scene.setFill(Color.TRANSPARENT);
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
         primaryStage.centerOnScreen();
         primaryStage.show();
+
+
+//        Parent root = FXMLLoader.load(this.getClass().getResource("view/LoginForm.fxml"));
+////        Parent root = FXMLLoader.load(this.getClass().getResource("view/MainForm.fxml"));
+//        Scene loginScene = new Scene(root);
+//        primaryStage.setScene(loginScene);
+//        primaryStage.setTitle("Login");
+//        primaryStage.setResizable(false);
+//        primaryStage.centerOnScreen();
+//        primaryStage.show();
     }
 }
